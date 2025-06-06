8ac6297a5a714c58cec1984ec5779082f597ee24
Joelfm4 <joelfmarques1@gmail.com>
2025-06-06 18:58:31 +0100

    images

--- src/ray_casting.h
+++ src/ray_casting.h
@@ -6,8 +6,8 @@ using namespace QuickCG;
 
 const int screenWidth = 800;
 const int screenHeight = 600;
-const int mapWidth = 31;
-const int mapHeight = 31;
+const int mapWidth = 17;
+const int mapHeight = 17;
 
 void runRayCasting(int worldMap[mapWidth][mapHeight]);
 
